f05287a01ff5d555943084dd5bc5f6ea93426b85
Conroar Boyer <conroarproductions@gmail.com>
2022-03-30 16:20:09 -0500

    new animations

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpAmount;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode attack;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKeyDown(jump) && isGrounded) //Jump
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(left)) //Left
+        {
+            rb.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
+
+        }
+        if (Input.GetKeyDown(right)) //Right
+        {
+            rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
